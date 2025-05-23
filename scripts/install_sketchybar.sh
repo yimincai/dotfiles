c84dea94c603d82ec819412fa179c6072b790265
yimincai <formosaneko.tw@gmail.com>
2025-05-23 10:51:22 +0800

    fix: remove usless files in arch

--- scripts/install_sketchybar.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/jpcrs/dotfiles.git /tmp/dotfiles
-cd /tmp/dotfiles && git checkout mac
-mv $HOME/.dotfiles/sketchybar $HOME/.dotfiles/sketchybar_backup
-mv /tmp/dotfiles/.config/sketchybar $HOME/.dotfiles/sketchybar
-rm -rf /tmp/dotfiles
-brew services restart sketchybar
